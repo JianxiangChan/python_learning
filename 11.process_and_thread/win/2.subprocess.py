524164145b88a94c211efb26783c4e14a3b98764
JianxiangChan <15651898806@163.com>
2019-06-20 23:25:46 +0800

    [w25.2][ShaneChen]1.use of subprocess

---
+++ 11.process_and_thread/win/2.subprocess.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+#!usr/bin/python
+
+import subprocess
+
+print('$ nslookup www.python.org')
+r = subprocess.call(['nslookup', 'www.python.org'])
+
+print('Exit code', r=0)
